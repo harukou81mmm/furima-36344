acb6526036e6a1eb58c25208e9cc6193d5ba0ad9
harukou81mmm <sanyouxidaheyuan@gmail.com>
2021-12-04 18:41:36 +0900

    指摘事項修正２回目

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,11 +4,11 @@ FactoryBot.define do
   
     name { Faker::Name.name }
     item_condition { Faker::Lorem.sentence }
-    category_id { Faker::Number.between(from: 1, to: 10) }
-    status_id { Faker::Number.between(from: 1, to: 6) }
-    shopping_charges_id { Faker::Number.between(from: 1, to: 2) }
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    status_id { Faker::Number.between(from: 2, to: 7) }
+    shopping_charges_id { Faker::Number.between(from: 2, to: 3) }
     shopping_area_id { Faker::Number.between(from: 1, to: 47) }
-    day_id { Faker::Number.between(from: 1, to: 3) }
+    day_id { Faker::Number.between(from: 2, to: 4) }
     price { Faker::Number.between(from: 300, to: 9_999_999) }
 
     after(:build) do |item|
