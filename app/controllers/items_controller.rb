36f6351f0f6f767787fef13fa00a1cadab485243
harukou81mmm <sanyouxidaheyuan@gmail.com>
2021-12-11 15:55:15 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
 
-
   def index
     @items = Item.order('created_at DESC')
   end
@@ -24,9 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless @item.user_id == current_user.id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless @item.user_id == current_user.id
   end
 
   def update
