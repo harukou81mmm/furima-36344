050b8401889732e87c5142af81d3b9e075259a63
harukou81mmm <sanyouxidaheyuan@gmail.com>
2021-12-11 12:40:46 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,12 +36,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    #if @item.user_id == current_user.id
-      #@item.destroy
-      #redirect_to root_path
-    #else
-      #redirect_to root_path
-    #end
+    # if @item.user_id == current_user.id
+    # @item.destroy
+    # redirect_to root_path
+    # else
+    # redirect_to root_path
+    # end
   end
 
   private
@@ -50,5 +50,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :item_condition, :category_id, :status_id, :shopping_charges_id,
                                  :shopping_area_id, :day_id, :price).merge(user_id: current_user.id)
   end
-  
 end
